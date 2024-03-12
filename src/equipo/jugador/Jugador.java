9c8ff0e00e220abd57235dce9c138eb572485ff4
davidMarin <dmarlop113@g.educaand.es>
2024-03-12 13:58:20 +0100

    clase abstracta jugador creada

--- src/equipo/jugador/Jugador.java
+++ src/equipo/jugador/Jugador.java
@@ -1,5 +1,7 @@
 package equipo.jugador;
 
+import actividad1.zoo.Animal;
+
 public abstract class Jugador {
 
 	//nombre, dorsal y equipo.
@@ -9,6 +11,56 @@ public abstract class Jugador {
 	private String equipo;
 	
 	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getDorsal() {
+		return dorsal;
+	}
+
+	public void setDorsal(int dorsal) {
+		this.dorsal = dorsal;
+	}
+
+
+
+	public String getEquipo() {
+		return equipo;
+	}
+
+
+
+	public void setEquipo(String equipo) {
+		this.equipo = equipo;
+	}
+
+
+
 	public abstract void mostrarDatos();
+	
+	@Override
+	public String toString() {
+		return "Jugador [nombre=" + this.nombre + ", dorsal y equipo: " + this.equipo + "]";
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		Jugador nuevoJugador = (Jugador) obj;
+
+		boolean result = false;
+
+		if (this.nombre == nuevoJugador.nombre && this.dorsal == nuevoJugador.dorsal && this.equipo == nuevoJugador.equipo) {
+			result = true;
+		}
+		return result;
+	}
+	
+}
+
 
 }
