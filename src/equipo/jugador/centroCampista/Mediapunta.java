a26e66555cadd458938b9ca8e6a49a5a36eb3d1f
MarioMoreno37 <mrme3773@hotmail.com>
2024-03-18 20:03:51 +0100

    Creacion de la clase Mediapunta

---
+++ src/equipo/jugador/centroCampista/Mediapunta.java
@@ -0,0 +1,42 @@
+package equipo.jugador.centroCampista;
+
+
+public class Mediapunta extends CentroCampista {
+    private int asistencias;
+
+    public Mediapunta(String nombre, int dorsal, String equipo,int pasesCompletados, int asistencias) {
+        super(nombre, dorsal, equipo, pasesCompletados);
+        this.asistencias = asistencias;
+    }
+
+    public int getAsistencias() {
+        return asistencias;
+    }
+
+    public void setAsistencias(int asistencias) {
+        this.asistencias = asistencias;
+    }
+
+    @Override
+    public void mostrarDatos() {
+        System.out.println("Nombre: " + getNombre());
+        System.out.println("Dorsal: " + getDorsal());
+        System.out.println("Equipo: " + getEquipo());
+        System.out.println("Asistencias: " + getAsistencias());
+    }
+
+    @Override
+    public String toString() {
+        return "Mediapunta [nombre=" + getNombre() + ", dorsal y equipo: " + getEquipo() + ", asistencias=" + asistencias + "]";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof Mediapunta))
+            return false;
+        Mediapunta otroMediapunta = (Mediapunta) obj;
+        return getNombre().equals(otroMediapunta.getNombre()) && getDorsal() == otroMediapunta.getDorsal() && getEquipo().equals(otroMediapunta.getEquipo()) && asistencias == otroMediapunta.asistencias;
+    }
+}
